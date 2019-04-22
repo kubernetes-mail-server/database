575e1378256a721a1f39a82f4d2f0286654151ef
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-04-23 00:34:57 +0200

    add the ability to map from a standard name to the PHINX name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
+for e in $(env | grep ^DATABASE_); do
+    export -p $(echo $e | sed 's/^DATABASE_/PHINX_/');
+done
+
 [ -z "${PHINX_HOSTNAME}" ] && echo "PHINX_HOSTNAME was not configured" && exit 0
 [ -z "${PHINX_PORT}" ] && PHINX_PORT=3306
 [ -z "${PHINX_ENCODING}" ] && PHINX_ENCODING=utf8
 
 until nc -v -z ${PHINX_HOSTNAME} ${PHINX_PORT}; do
-    echo "Connecting to Mysql..."
+    echo "Connecting to Mysql ${PHINX_HOSTNAME}:${PHINX_PORT}..."
     sleep 2
 done
 
