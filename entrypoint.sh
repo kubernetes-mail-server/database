4bd2ba4ee8db8873fe86da2f877d0e1d7c6b532a
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-04-23 00:46:17 +0200

    update the database to use the global database configurations as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,11 @@ for e in $(env | grep ^DATABASE_); do
     export -p $(echo $e | sed 's/^DATABASE_/PHINX_/');
 done
 
+# patch up the mismatch of naming conventions
+PHINX_DATABASE=${DATABASE_NAME}
+
 [ -z "${PHINX_HOSTNAME}" ] && echo "PHINX_HOSTNAME was not configured" && exit 0
+[ -z "${PHINX_DATABASE}" ] && echo "PHINX_DATABASE was not configured" && exit 0
 [ -z "${PHINX_PORT}" ] && PHINX_PORT=3306
 [ -z "${PHINX_ENCODING}" ] && PHINX_ENCODING=utf8
 
