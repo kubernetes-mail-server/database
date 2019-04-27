d6d80f5ea770996e6d0fb5642cf644a02cf3364f
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-04-27 14:05:27 +0200

    I really need a better hobby

--- migrations/20190426181900_simplify_aliases.php
+++ migrations/20190426181900_simplify_aliases.php
@@ -7,12 +7,14 @@ class SimplifyAliases extends AbstractMigration
 	{
 		$table = $this->table("virtual_aliases");
 
-        $table->addColumn("dst_email", "string", ["limit" => 255, "null" => false])->save();
-
 		$table->dropForeignKey("user_id")->save();
 		$table->removeColumn("user_id")->save();
 
 		$table->renameColumn("email", "src_email")->save();
-		$table->changeColumn("src_email", "string", ["limit" => 255, "null" => false])->save();
+
+		$table
+            ->changeColumn("src_email", "string", ["limit" => 255, "null" => false])
+            ->addColumn("dst_email", "string", ["limit" => 255, "null" => false])
+            ->save()
 	}
 }
