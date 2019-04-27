1ae45070dedbdca3ec7ab23c0c4fd27b66ae9339
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-04-27 12:43:26 +0200

    try to separate these commands so they execute correctly

--- migrations/20190426181900_simplify_aliases.php
+++ migrations/20190426181900_simplify_aliases.php
@@ -6,18 +6,12 @@ class SimplifyAliases extends AbstractMigration
 	public function change()
 	{
 		$table = $this->table("virtual_aliases");
-		$table
-			->dropForeignKey("user_id")
-			->removeColumn("user_id")
-            ->save();
 
-		$table
-            ->renameColumn("email", "src_email")
-            ->save();
+		$table->dropForeignKey("user_id")->save();
+		$table->removeColumn("user_id")->save();
+		$table->renameColumn("email", "src_email")->save();
 
-		$table
-			->changeColumn("src_email", "string", ["limit" => 255, "null" => false])
-			->addColumn("dst_email", "string", ["limit" => 255, "null" => false])
-			->save();
+		$table->changeColumn("src_email", "string", ["limit" => 255, "null" => false])->save();
+		$table->addColumn("dst_email", "string", ["limit" => 255, "null" => false])->save();
 	}
 }
